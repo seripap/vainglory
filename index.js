e81d2538769a8ad41cc9934cfc142ae02ccdcb58
Dan Seripap <daniel@seripap.com>
2016-12-06 13:22:13 -0500

    Fixed prepublish script, actually exporting now.

--- index.js
+++
@@ -1,4 +0,0 @@
-require('babel-polyfill');
-require('./src/Vainglory.js');
-
-console.log('Warning! The Vainglory API is still in development and is prebaked, like this module. Use with caution.');
--- src/Vainglory.js
+++ src/Vainglory.js
@@ -1,16 +1,21 @@
+require('babel-polyfill');
+
 import Api from './api';
 import HTTP from './Http';
 
-export default class Vainglory {
+class Vainglory {
   constructor(apiKey = null) {
     if (!apiKey) {
       throw new Error('Missing API Key.');
     }
 
     const api = new Api(new HTTP(apiKey));
+    
     // Exposed methods
     this.meta = api.meta;
     this.matches = api.matches;
   }
 
 }
+
+module.exports = Vainglory;
